b27e9d3d0054bab9f696c183b995cd4c3732a1d4
matcharr <you@example.com>
2020-07-15 20:24:48 +0200

    crypto done, error done

---
+++ lib/crypto.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'open-uri'
+
+def crypto
+  pages = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))
+  tmp = page.css('/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr')
+  crypto_name_array = (tmp.map { |tr| { tr.css('td[2]/div/a').text => tr.css('td[5]/a').text } })
+  pp crypto_name_array
+end
+
+begin
+  crypto
+rescue StandardError => e
+  puts 'You messed up in crypto!'
+end
