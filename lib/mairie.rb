42c46d145144cce9ea4d2a12fa5ad4c1f8a3b628
matcharr <you@example.com>
2020-07-16 16:57:00 +0200

    test

--- lib/mairie.rb
+++ lib/mairie.rb
@@ -1,21 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
-def mairie
-  url1 = 'http://annuaire-des-mairies.com/val-d-oise.html'
-  page = Nokogiri::HTML(URI.open(url1))
-  tmp = page.css('[@class="lientxt"]')
-  tmp.each do |nom|
-    nomCommune = nom['href'].delete_prefix '.'
-    url2 = "http://annuaire-des-mairies.com#{nomCommune}"
-    page2 = Nokogiri::HTML(URI.open(url2))
-    email = page2.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-    pp email
-    pp nomCommune
-  end
+
+def get_email(nom)
+	nomCommune = nom['href'].delete_prefix "." 
+	page2 = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com#{nomCommune}"))
+	email = page2.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+end
+
+def mairie_christmas
+	pages = Nokogiri::HTML(URI.open('http://annuaire-des-mairies.com/val-d-oise.html'))
+	temp = pages.css('[@class="lientxt"]')
+	tab = temp.map { |nom| { nom.text => get_email(nom)} }
 end
 
 begin
-  mairie
-rescue StandardError => e
-  puts 'You messed up in mairie!'
+	pp mairie_christmas
+rescue => e
+	puts 'You messed up in mairie_christmas!'
 end
--- spec/mairie_spec.rb
+++ spec/mairie_spec.rb
@@ -1,16 +1,12 @@
 require_relative "../lib/mairie"
 
-describe "Crypto fonction" do
-    it "Crypto should display an array without nil." do
-        expect(.is_a? Array).to eq true
-        expect(crypto.all? { |elem| elem.class == Hash}).to be true
-    end
-    it "Crypto must have string keys Bitcoin and Ethereum." do 
-        
-        expect((crypto[0]).key?("Bitcoin")).to eq true
-        expect((crypto[1]).key?("Ethereum")).to eq true
-    end
-    it "Crypto must have 200 elements." do
-        expect(crypto.count).to eq(200)
-    end
+describe "mairie_christmas fonction" do
+		it "Mairie should display an array whitout nil" do
+				expect(mairie_christmas.is_a? Array).to eq true
+				expect(mairie_christmas.all? { |elem| elem.class == Hash}).to be true
+		end
+		it "Mairie has a least few city" do 
+				expect((mairie_christmas[0]).key?("ABLEIGES")).to eq true
+				expect((mairie_christmas[1]).key?("AINCOURT")).to eq true
+		end
 end
