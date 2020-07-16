37e7f5057f2179f22cae3c55d23b6c39e2ab07de
matcharr <you@example.com>
2020-07-16 13:02:32 +0200

    test

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,13 +1,22 @@
 require_relative "../lib/crypto"
 
-describe "the dark_trader fonction" do
-    it "should display an array" do
-        expect((crypto)).to be_a_kind_of Array 
+describe "Crypto fonction" do
+    it "Crypto should display an array without nil." do
+        expect(crypto.is_a? Array).to eq true
         expect((crypto)).not_to eq nil
     end
-    it "should at least contain Bitcoin and Ethereum" do 
+    it "Crypto key and value are string." do
+        expect((crypto[0]).is_a? String).to eq true
+        expect((crypto[1]).is_a? String).to eq true
+        expect(((crypto[0].values(1)).is_a? Numeric)).to eq true
+        expect(((crypto[0].key(0).is_a? Numeric))).to eq true
+    end
+    it "Crypto must have string keys Bitcoin and Ethereum." do 
         
         expect((crypto[0]).key?("Bitcoin")).to eq true
         expect((crypto[1]).key?("Ethereum")).to eq true
+    end
+    it "Crypto must have 200 elements." do
+        expect(crypto.count).to eq(200)
     end
 end
